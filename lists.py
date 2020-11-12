6ba573f89160018f584c82fafe484c4add9a8e85
Aidan Milbourne <amilbourne@thegraysonschool.org>
2020-11-12 19:54:37 +0000

    everything

--- lists.py
+++ lists.py
@@ -1,11 +1,24 @@
 def sum_of_odd_nums(n):
-    pass
+    odd_nums=[i for i in range(1,2*n,2)]
+    return (sum(odd_nums))
 
 def caesar_cipher(message, key):
-    pass
+    character_list=[ord(i)+key for i in (message)]
+    character_list=bytes(character_list)
+    return character_list.decode('ascii')
 
 def fizzbuzz(n):
-    pass 
+    fizzbuzz=[fizzerbuzzer(i) for i in range(1,n+1)]
+    return fizzbuzz
+
+def fizzerbuzzer(n):
+    if n//15==n/15:
+      return "fizzbuzz"
+    elif n//5==n/5:
+      return "buzz"
+    elif n//3==n/3:
+      return "fizz"
+    return n
 
 def main():
     print('Table of the sum for the first n odd numbers:')
